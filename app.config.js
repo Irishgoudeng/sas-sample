6e26f66f691a0b17b3440d4326e55be5de7ce895
GMJSilmaro <dzheysilmaro@gmail.com>
2024-10-02 10:16:52 +0800

    update firebase.js, login.js

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export const settings = {
     app: {
-        name: "Potensi",
-        version: "0.0.1"
+        name: "SAS",
+        version: "0.0.2"
     },
     theme: {
         skin: "light" // possible values are light, dark
