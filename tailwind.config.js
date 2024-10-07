9be41f0bc42c8ad168547d2602885c21e61d9b21
GMJSilmaro <dzheysilmaro@gmail.com>
2024-10-07 22:44:03 +0800

    Added Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './pages/**/*.js',
+    './components/**/*.js',
+    './styles/**/*.scss',  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
