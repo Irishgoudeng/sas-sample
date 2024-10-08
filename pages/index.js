c66631e4e76d7ca6eef330b215e76923bf01af9a
irishgoudeng <vincewarpie@gmail.com>
2024-10-08 11:25:29 +0800

    Added Possible Fixes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import { Fragment, useEffect } from 'react';
-import 'react-toastify/dist/ReactToastify.css';
+import { Fragment, useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/theme/_theme.scss"; // Adjust the path as necessary
 
 const Home = () => {
-
-
-  return <Fragment>
-  </Fragment>;
+  return <Fragment></Fragment>;
 };
 
 export default Home;
